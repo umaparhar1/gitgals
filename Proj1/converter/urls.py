fa43b0abff7d9cb126b87a7c2da02c36f739d8fd
Uma Parhar <umaparhar@umas-mbp.lan>
2020-06-25 17:12:12 -0500

    changed to index.html part 2

--- Proj1/converter/urls.py
+++ Proj1/converter/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mysite.html', views.home, name='converter-home'),
+    path('index.html', views.home, name='converter-home'),
     path('aboutCreators.html', views.creators, name='converter-creators'),
     path('ourPortfolio.html', views.portfolio, name='converter-portfolio'),
     path('converter.html', views.converter, name='converter-app'),
