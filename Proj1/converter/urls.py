51a3ffa01c006c6b70957d8fd260f00065889b4e
liferg <45667185+liferg@users.noreply.github.com>
2020-07-14 17:53:33 -0500

    first commit

---
+++ Proj1/converter/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index.html', views.home, name='converter-home'),
+    path('aboutCreators.html', views.creators, name='converter-creators'),
+    path('ourPortfolio.html', views.portfolio, name='converter-portfolio'),
+    path('converter.html', views.converter, name='converter-app'),
+    path('workout.html', views.workout, name='workout-app'),
+    path('bmi.html', views.bmi, name='bmi-app'),
+    path('caloriesBurned.html', views.caloriesBurned, name='caloriesBurned-app'),
+]
---
+++ Proj1/converter/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'converter/index.html', {})
+
+def creators(request):
+    return render(request, 'converter/aboutCreators.html', {})
+
+def portfolio(request):
+    return render(request, 'converter/ourPortfolio.html', {})
+
+def converter(request):
+    return render(request, 'converter/converter.html', {})
+
+def workout(request):
+    return render(request, 'converter/workout.html', {})
+
+def bmi(request):
+    return render(request, 'converter/bmi.html', {})
+
+def caloriesBurned(request):
+    return render(request, 'converter/caloriesBurned.html', {})
