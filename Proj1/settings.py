cd4d02c5ff0ecdff896065da21d07294987b25c2
Uma Parhar <umaparhar@Umas-MBP.hsd1.mn.comcast.net>
2020-07-20 19:52:39 -0500

    heroku working

--- Proj1/settings.py
+++ Proj1/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gf4@7*oo^1u8q)deyt9s42=%kdj9yd%15x-f5ir3#4x*_&q=by'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['gitgals.herokuapp.com']
 
 # Application definition
 
@@ -118,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
